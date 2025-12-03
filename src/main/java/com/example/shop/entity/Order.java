9551f7a163cf65fe1598f3f0cffa41399be7ba5a
zyyx-linhnh <linhnh@zyyx.jp>
2025-12-03 10:51:54 +0700

    config connect to workbench

--- src/main/java/com/example/shop/entity/Order.java
+++ src/main/java/com/example/shop/entity/Order.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
